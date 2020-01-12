cad9695e872bdb0fce362005a8d89d8f3fa63c80
2000me <meggers@hmac.fritz.box>
2020-01-12 12:43:46 +0100

    localization script

---
+++ js/i18n.js
@@ -0,0 +1,14 @@
+const translate = (function() {
+  const nodes = document.querySelectorAll("[data-i18n]");
+  nodes.forEach(
+    function(node) {
+      node.innerHTML = browser.i18n.getMessage(
+        node.getAttribute('data-i18n')
+      )
+    }
+  )
+});
+
+window.addEventListener('DOMContentLoaded', (event) => {
+    translate();
+});
