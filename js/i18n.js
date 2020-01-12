42b8943401e4e65b54d7af77e6bbc75622fcda42
2000me <meggers@hmac.fritz.box>
2020-01-12 16:55:14 +0100

    sanitize innerHTML

--- js/i18n.js
+++ js/i18n.js
@@ -2,7 +2,7 @@ const translate = (function() {
   const nodes = document.querySelectorAll("[data-i18n]");
   nodes.forEach(
     function(node) {
-      node.innerHTML = browser.i18n.getMessage(
+      node.textContent = browser.i18n.getMessage(
         node.getAttribute('data-i18n')
       )
     }
